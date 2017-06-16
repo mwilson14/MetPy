01b89e1f6e8a88583864854435d1c76bb8455edf
Matt Wilson <matthew.wilson@valpo.edu>
2017-06-16 14:24:16 -0500

    Removed some spaces and fixed intro

--- metpy/calc/tests/test_kinematics.py
+++ metpy/calc/tests/test_kinematics.py
@@ -368,7 +368,7 @@ def test_geostrophic_gempak():
 
 
 def test_helicity():
-    """Test function for SRH calculations on a quarter-circle hodograph"""
+    """Test function for SRH calculations on a quarter-circle hodograph."""
     pres_test = np.asarray([1013.25, 954.57955706, 898.690770743, 845.481604002, 794.85264282])
     hgt_test = hgt_test = np.asarray([0, 500, 1000, 1500, 2000])
     # Create larger arrays for everything except pressure to make a smoother graph
@@ -376,21 +376,21 @@ def test_helicity():
     dir_int = np.arange(180, 272.25, 2.25)
     spd_int = np.zeros((hgt_int.shape[0]))
     spd_int[:] = 2.
-    u_int, v_int = get_wind_components(spd_int * units('m/s'), dir_int * units.degree)
+    u_int, v_int = get_wind_components(spd_int * units('m/s'),dir_int * units.degree)
     # Interpolate pressure to that graph
-    pres_int = np.interp(hgt_int, hgt_test, np.log(pres_test))
+    pres_int = np.interp(hgt_int,hgt_test,np.log(pres_test))
     pres_int = np.exp(pres_int)
     # Put in the correct value of SRH for a quarter-circle, 2 m/s hodograph
     # (SRH = 2 * area under hodo, in this case...)
-    SRH_true_p = 2 * (.25 * np.pi * (2 ** 2)) * units('m^2/s^2')
+    srh_true_p = 2 * (.25 * np.pi * (2 ** 2)) * units('m^2/s^2')
     # Since there's only positive SRH in this case, total SRH will be equal to positive SRH and
     # negative SRH will be zero.
-    SRH_true_t = SRH_true_p
-    SRH_true_n = 0 * units('m^2/s^2')
+    srh_true_t = SRH_true_p
+    srh_true_n = 0 * units('m^2/s^2')
     p_srh, n_srh, T_srh = storm_relative_helicity(u_int,v_int,pres_int,2000,hgt_int,
-                                                  srh_bottom = 0,storm_u = 0 * units.knot,
-                                                  storm_v = 0 * units.knot,
-                                                  dp =-1,exact = False)
-    assert_almost_equal(p_srh,SRH_true_p, 2)
-    assert_almost_equal(n_srh,SRH_true_n, 2)
-    assert_almost_equal(T_srh,SRH_true_t, 2)
+                                                  srh_bottom=0,storm_u=0 * units.knot,
+                                                  storm_v=0 * units.knot,
+                                                  dp=-1,exact=False)
+    assert_almost_equal(p_srh,srh_true_p, 2)
+    assert_almost_equal(n_srh,srh_true_n, 2)
+    assert_almost_equal(T_srh,srh_true_t, 2)
