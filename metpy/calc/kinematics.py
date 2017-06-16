992a0416a4a433f21f9f673b1e08d6e4f79e4a1e
Matt Wilson <matthew.wilson@valpo.edu>
2017-06-16 13:56:21 -0500

    Fixed a few more formatting issues

--- metpy/calc/kinematics.py
+++ metpy/calc/kinematics.py
@@ -438,9 +438,9 @@ def geostrophic_wind(heights, f, dx, dy):
 @exporter.export
 @check_units('[speed]', '[speed]', '[pressure]', '[length]',
              '[length]', '[length]', '[speed]', '[speed]')
-def storm_relative_helicity(u, v, p, srh_top, hgt, srh_bottom=0, storm_u=0*units('m/s'),
-                   storm_v=0*units('m/s'), dp=-1, exact=True):
-    r"""Calculates Storm Relative Helicity.
+def storm_relative_helicity(u, v, p, srh_top, hgt, srh_bottom=0, storm_u=0 * units('m/s'),
+                   storm_v=0 * units('m/s'), dp=-1, exact=True):
+    r"""Calculate Storm Relative Helicity.
 
     Needs u and v wind components, heights and pressures,
     and top and bottom of SRH layer. An optional storm
